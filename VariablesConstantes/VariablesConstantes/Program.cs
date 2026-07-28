47565a3fccf89b76281480840f78b8ffd4f48d86
Fractalyc <nicolasj.cabana@gmail.com>
2026-07-28 11:31:36 -0500

    Este es mi primer commit
    NJCR 202602

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Nicolás Cabana";
+            Console.WriteLine("Bienvenido, " + nombre + ", al curso de fundamentos de programación.");
+        }
+    }
+}
