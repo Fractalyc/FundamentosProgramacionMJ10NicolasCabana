7745a6c9246c0b733aa285d87dd30587fe08aff8
Fractalyc <nicolasj.cabana@gmail.com>
2026-08-13 11:30:22 -0500

    6.CondicionalesMultiples
    (switch, break, default)

---
+++ 6.CondicionalesMultiplesNJCR/6.CondicionalesMultiplesNJCR/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _6.CondicionalesMultiplesNJCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int respuesta = 0;
+            //Condicionales múltiples
+            //Cree un algortimo donde se elija una de 5 opciones, error si no se detecta un valor válido
+            Console.WriteLine("------------Menú-----------");
+            Console.WriteLine("1. Opción 1     2. Opción 2");
+            Console.WriteLine("3. Opción 3     4. Opción 4");
+            Console.WriteLine("5. Opción 5");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Seleccione el número de UNA opción.");
+            respuesta = int.Parse(Console.ReadLine());
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Eligió la Opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligió la Opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligió la Opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligió la Opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligió la Opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción válida");
+                    break;
+            }
+        }
+    }
+}
---
+++ 6.CondicionalesMultiplesNJCR/6.CondicionalesMultiplesNJCR/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("6.CondicionalesMultiplesNJCR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6.CondicionalesMultiplesNJCR")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("869b8362-1dbe-4f0b-9f5a-2a2d95424163")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
