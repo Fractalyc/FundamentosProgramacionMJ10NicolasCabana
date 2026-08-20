7d919dbf56192427064a092422db2f70f5147d27
Fractalyc <nicolasj.cabana@gmail.com>
2026-08-19 22:45:08 -0500

    LocalDesktopCopy

--- Taller1ProgramPreparacionNJCR/Taller1ProgramPreparacionNJCR/Program.cs
+++ Taller1ProgramPreparacionNJCR/Taller1ProgramPreparacionNJCR/Program.cs
@@ -89,14 +89,80 @@ namespace Taller1ProgramPreparacionNJCR
 
             //-------------------------------------------------------------PUNTO 3--------------------------------------------------------------------
 
-            int p1x = 0;
+            /*int p1x = 0;
             int p1y = 0;
             int p2x = 0;
             int p2y = 0;
             int p3x = 0;
             int p3y = 0;
 
+            //We ask for the points needed for the triangle
+            Console.WriteLine("Let's see if we can make a triangle with 3 points on a plane!");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.Write("Please input coordinate X of the first point: ");
+            p1x = int.Parse(Console.ReadLine());
+            Console.Write("Please input coordinate Y of the first point: ");
+            p1y = int.Parse(Console.ReadLine());
+            Console.Write("Please input coordinate X of the second point: ");
+            p2x = int.Parse(Console.ReadLine());
+            Console.Write("Please input coordinate Y of the second point: ");
+            p2y = int.Parse(Console.ReadLine());
+            Console.Write("Please input coordinate X of the third point: ");
+            p3x = int.Parse(Console.ReadLine());
+            Console.Write("Please input coordinate Y of the third point: ");
+            p3y = int.Parse(Console.ReadLine());
 
+            //We calculate the distances between each point, in  other words, the sides of the triangle
+            double lado12 = Math.Sqrt(((p2x - p1x) ^ 2) + ((p2y - p1y) ^ 2));
+            double lado13 = Math.Sqrt(((p3x - p1x) ^ 2) + ((p3y - p1y) ^ 2));
+            double lado23 = Math.Sqrt(((p3x - p2x) ^ 2) + ((p3y - p2y) ^ 2));
+
+            //Se puede hacer un triángulo?
+            if ((lado12 + lado13) > lado23 && (lado12 - lado13) < lado23)
+            {
+                if ((lado12 + lado23) > lado13 && (lado12 - lado23) < lado13)
+                {
+                    if ((lado13 + lado23) > lado12 && (lado13 - lado23) < lado12)
+                    {
+                        Console.WriteLine("It IS  possible to  make a triangle with this set of points");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Side A does not meet the porperties of a triangle");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("At least Side B does not meet the porperties of a triangle");
+                }
+            }
+            else
+            {
+                Console.WriteLine("At least Side C does not meet the porperties of a triangle");
+            }
+            */
+
+            //-------------------------------------------------------------PUNTO 4--------------------------------------------------------------------
+            //Uso "d" y "a" porque así es el movimiento WASD en los juegos.
+
+            string movimiento = "";
+            Console.WriteLine("Move your character");
+            Console.WriteLine("Press D to go to the right");
+            Console.WriteLine("Press A to go to the left");
+            movimiento = Console.ReadLine();
+
+            if (movimiento == "d" || movimiento == "D")
+            {
+                Console.WriteLine("The character is moving to the right");
+            }
+            else if (movimiento == "a" || movimiento == "A")
+            {
+                Console.WriteLine("The character is moving to the left");
+            }
+            else
+            {
+                Console.WriteLine("Invalid direction.");
+            }
         }
     }
 }
