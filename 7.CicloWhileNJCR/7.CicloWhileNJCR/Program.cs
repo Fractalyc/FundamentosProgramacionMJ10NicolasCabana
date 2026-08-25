0fe8be9d7e4bc7b44d3b60668601f3c82843eaf6
Fractalyc <nicolasj.cabana@gmail.com>
2026-08-25 11:28:19 -0500

    7.CiclosWhileEjercicios

---
+++ 7.CicloWhileNJCR/7.CicloWhileNJCR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace _7.CicloWhileNJCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador = acumulador + contador;
+                contador++;
+            }
+            Console.WriteLine("La suma de los 5 primeros enteros es: " + acumulador);*/
+
+
+            //Crear un algoritmo que permita generar el factorial de los primeros 5 números
+            /*int contador1 = 1;
+            int acumulador2 = 1;
+            int numero = 0;
+            Console.WriteLine("Por favor escriba un número positivo: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador1 <= numero)
+            {
+                acumulador2 = acumulador2 * contador1;
+                contador1++;
+            }
+            Console.WriteLine($"El factorial de {numero} es: " + acumulador2);*/
+
+
+        }
+    }
+}
