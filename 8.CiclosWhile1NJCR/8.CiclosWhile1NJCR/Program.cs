a13e864c2f27d21795aa0c5418daac4eb6734b00
Fractalyc <nicolasj.cabana@gmail.com>
2026-08-25 18:46:39 -0500

    8.CiclosWhile1NJCR

---
+++ 8.CiclosWhile1NJCR/8.CiclosWhile1NJCR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace _8.CiclosWhile1NJCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros = 0;
+            int suma = 0;
+
+            Console.WriteLine("Por favor ingrese números enteros");
+            Console.Write("El primer número: ");
+
+            while (numeros >= 0)
+            {
+                numeros = int.Parse(Console.ReadLine());
+                suma = suma + numeros;
+                if (numeros >= 0)
+                {
+                    Console.Write("Siguiente número: ");
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                }
+            }
+
+            Console.WriteLine("La suma de los números obtenidos es: " + suma);
+        }
+    }
+}
