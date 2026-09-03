421dd7688af7f7612a196b45e2907f104384853a
Fractalyc <nicolasj.cabana@gmail.com>
2026-09-03 11:21:47 -0500

    12.CiclosDoWhile

---
+++ 12.CiclosDoWhile/12.CiclosDoWhile/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace _12.CiclosDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string usuario = "";
+            int numeroDeCuenta = 0;
+            int saldo = 0;
+            int promedioSaldos = 0;
+            int iteraciones = 0;
+            int sumaDeSaldos = 0;
+            string deseaContinuar = "";
+            Console.WriteLine("Bienvenido, por favor ingrese los datos requeridos.");
+            do
+            {
+                Console.WriteLine("Ingrese su nombre: ");
+                usuario = Console.ReadLine();
+                Console.WriteLine("Ingrese su número de cuenta: ");
+                numeroDeCuenta = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su saldo: ");
+                saldo = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Hola, {usuario}, número de cuenta: {numeroDeCuenta} tiene un saldo de {saldo}.");
+                sumaDeSaldos += saldo;
+                if (saldo >= 3000000)
+                {
+                    Console.WriteLine("Es apto para el crédito.");
+                }
+                else
+                {
+                    Console.WriteLine("No es apto para el crédito.");
+                }
+                iteraciones++;
+                Console.WriteLine("Desea revisar otra cuenta? Responda SI para continuar.");
+                deseaContinuar = Console.ReadLine();
+            }
+            while (deseaContinuar == "SI");
+
+            promedioSaldos = sumaDeSaldos / iteraciones;
+            Console.WriteLine($"Revisó {iteraciones} cuentas.");
+            Console.WriteLine($"El promedio de los saldos entre las cuentas fue: {promedioSaldos}.");*/
+
+            //adivina la contraseña
+
+            const string password = "Manila";
+            string passwordAtt = "";
+            int passAttNum = 0;
+            
+            do
+            {
+                Console.WriteLine("Ingrese la contraseña correcta: ");
+                passwordAtt = Console.ReadLine();
+                passAttNum++;
+                if (passwordAtt != password)
+                {
+                    Console.WriteLine("Incorrecto, intente de nuevo.");
+                }
+            }
+            while (passwordAtt != password);
+
+            Console.WriteLine("Contraseña correcta");
+            Console.WriteLine($"Intentos = {passAttNum}");
+        }
+    }
+}
---
+++ 12.CiclosDoWhile/12.CiclosDoWhile/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("12.CiclosDoWhile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12.CiclosDoWhile")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("67537dd8-988f-40af-b0e0-09fd803184a3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
