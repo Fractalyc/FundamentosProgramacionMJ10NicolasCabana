e6cc5e8c545cde265ac7177ee7f4fee297aa3e1d
Fractalyc <nicolasj.cabana@gmail.com>
2026-09-03 11:28:28 -0500

    13FinishAtHome

---
+++ 13.CiclosFor/13.CiclosFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13.CiclosFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo For
+            int acumulador = 0;
+
+            //Se puede usar "i" como si fuera la inicialización de una sumatoria, porque eso es lo que realmente es.
+
+            for (int i = 1; i <= 5; i++)
+            {
+                acumulador += i;
+            }
+            Console.WriteLine($"La suma de los primeros 5 enteros es {acumulador}");
+        }
+    }
+}
